086cc0362681742d8720e903615739ae5039f417
cha1ra <cha1ra.garnet@gmail.com>
2021-03-08 21:11:44 +0900

    feat: startを押すとクリックが開始する

---
+++ components/Metronome.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+  >
+    <v-card-text class="pt-12">
+      <v-slider
+        v-model="tempo"
+        thumb-label="always"
+        max="240"
+        min="40"
+      />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        class="font-weight-bold"
+        @click="setClicking(true)"
+      >
+        開始
+      </v-btn>
+      <v-btn
+        color="error"
+        class="font-weight-bold"
+        @click="setClicking(false)"
+      >
+        停止
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      tempo: 60,
+      clicking: false
+    }
+  },
+  methods: {
+    setClicking (flag) {
+      this.clicking = flag
+      flag && this.click()
+    },
+    click () {
+      console.log('click!')
+      this.clicking && setTimeout(this.click, 1000)
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <v-container>
-
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4">
+        <Metronome />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+<script>
+import Metronome from '../components/Metronome'
+export default {
+  components: {
+    Metronome
+  }
+}
+</script>
