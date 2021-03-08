489df3053d1137ff6151397e84f5926a448097b4
cha1ra <cha1ra.garnet@gmail.com>
2021-03-08 21:48:20 +0900

    feat: メトロノーム機能

--- components/Metronome.vue
+++ components/Metronome.vue
@@ -10,21 +10,28 @@
         min="40"
       />
     </v-card-text>
-    <v-card-actions>
-      <v-btn
-        color="primary"
-        class="font-weight-bold"
-        @click="setClicking(true)"
+    <v-card-actions class="d-flex justify-space-between">
+      <div>
+        <v-btn
+          color="primary"
+          class="font-weight-bold"
+          @click="setClicking(true)"
+        >
+          開始
+        </v-btn>
+        <v-btn
+          color="error"
+          class="font-weight-bold"
+          @click="setClicking(false)"
+        >
+          停止
+        </v-btn>
+      </div>
+      <div
+        :class="{'flash-light__active': flash}"
       >
-        開始
-      </v-btn>
-      <v-btn
-        color="error"
-        class="font-weight-bold"
-        @click="setClicking(false)"
-      >
-        停止
-      </v-btn>
+        ●
+      </div>
     </v-card-actions>
   </v-card>
 </template>
@@ -34,10 +41,26 @@ export default {
     return {
       tempo: 60,
       clicking: false,
-      timeoutId: null
+      flash: false,
+      timeoutId: null,
+      oscillator: null
     }
   },
+  computed: {
+    offsetMs () {
+      return Math.round(1000 * 60 / this.tempo)
+    }
+  },
+  mounted () {
+    this.initOscillator()
+  },
   methods: {
+    initOscillator () {
+      const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+      this.oscillator = audioCtx.createOscillator()
+      this.oscillator.frequency.value = 440
+      this.oscillator.connect(audioCtx.destination)
+    },
     setClicking (flag) {
       this.clicking = flag
       if (flag) {
@@ -48,11 +71,25 @@ export default {
       }
     },
     click () {
-      console.log('click!')
+      this.flashLight()
       if (this.clicking) {
-        this.timeoutId = setTimeout(this.click, 1000)
+        this.timeoutId = setTimeout(this.click, this.offsetMs)
       }
+    },
+    flashLight () {
+      this.flash = true
+      this.oscillator.start()
+      setTimeout(() => {
+        this.flash = false
+        this.oscillator.stop()
+        this.initOscillator()
+      }, 80)
     }
   }
 }
 </script>
+<style scoped lang="scss">
+  .flash-light__active {
+    color: orangered;
+  }
+</style>
